4626eada4fa151da4b58448825c22c3bad20654c
ViniWixak <viniciuswixak@gmail.com>
2024-01-26 17:38:18 -0300

    Working with useStates hookies

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,38 @@
+import { useState, useStates } from 'react'
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log("Cadastrou o usuário.")
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu Cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <label htmlFor="name"></label>
+                    <input 
+                        type="text"
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite o seu nome"                        
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password"></label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Digite a sua senha" 
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" placeholder="Cadastrar" />
